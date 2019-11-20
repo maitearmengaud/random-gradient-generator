1870b559d611250c193f848ff2ab0c2c1c231042
Arm <armengaud.m@gmail.com>
2019-11-20 20:33:50 +0100

    update style.css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+ const basicColor = {
+    hex: 'fff8dc',
+    name: 'color name' 
+ };
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        colors: [basicColor]
+      };
+      this.getColors = this.getColors.bind(this);
+  }
+
+  getColors () {
+    axios
+      .get("http://www.colr.org/json/colors/random/7")
+      .then(response => {
+        const {colors} = this.state;
+        colors.push(response.data.colors[0])
+        this.setState({ colors : response.data.colors });
+      })
+    .catch(error => console.log( error ));
+  }
+
+  componentDidMount() {
+    this.getColors();
+  }
+
+  render() {
+    const { colors } = this.state;
+    return (
+      <div className="App"
+        style={{
+          background:`linear-gradient(#${colors[0].hex},#${colors[0].hex})`
+        }}
+      >
+        <p>{colors[0].tags ? colors[0].tags[0].name:""}</p>
+        <button
+        style={{
+          color:`#${colors[0].hex}`
+        }}
+        onClick={this.getColors}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          RANDOM
+        </button>
+        <p>{colors[0].tags ? colors[1].tags[0].name:""}</p>
+      </div>
+    );
+  }
 }
-
 export default App;
